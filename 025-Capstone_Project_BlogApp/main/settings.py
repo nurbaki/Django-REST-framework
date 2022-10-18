030b2e0e0dce9963313b66bd3bfa48aac1bb9e36
nurbaki <nurbakibayansal11@gmail.com>
2022-10-18 10:54:42 +0200

    sdfvsf

--- 025-Capstone_Project_BlogApp/main/settings.py
+++ 025-Capstone_Project_BlogApp/main/settings.py
@@ -134,9 +134,3 @@ MEDIA_URL = 'media_url/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-#     ]
-# }
